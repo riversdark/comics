2de023baf8bab87da6baeca6d4d260a6ea9c8696
riversdark <olivier.ma.dq@gmail.com>
2023-10-13 10:31:51 +0800

    update ump

--- comics/_modidx.py
+++ comics/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://riversdark.github.io',
                 'git_url': 'https://github.com/riversdark/comics',
                 'lib_path': 'comics'},
-  'syms': {'comics.core': {'comics.core.foo': ('core.html#foo', 'comics/core.py')}}}
+  'syms': { 'comics.core': {'comics.core.foo': ('core.html#foo', 'comics/core.py')},
+            'comics.iam': {},
+            'comics.ump': {'comics.ump.process_files': ('ump.html#process_files', 'comics/ump.py')}}}
